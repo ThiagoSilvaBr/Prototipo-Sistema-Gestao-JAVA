bd283bb3dcce8a2e8374b19811fe5a17466ae689
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-05-11 16:43:58 -0300

    feat: cria modelagem da entidade Peca

---
+++ src/models/Peca.java
@@ -0,0 +1,57 @@
+package models;
+
+public class Peca {
+
+    private Long id;
+    private String nome;
+    private int quantidade;
+    private double precoUnitario;
+
+
+    public Peca() {
+    }
+
+    public Peca(Long id, String nome, int quantidade, double precoUnitario) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double calcularValorTotal(){
+        return quantidade * precoUnitario;
+    }
+
+}
